db2d6a450befec36ce929534d1ba273032352100
Benjamin Aplin <benjaminaplin@gmail.com>
2015-11-12 12:15:58 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 console.log('main is linked');
 
 $('div.eventr').on('mouseover', function(e){
-  $('img#eventr').css('opacity','0.3');
   $('#eventr-modal').toggle();
 })
 
-$('div.eventr').on('mouseout', function(e){
-  $('img#eventr').css('opacity','1');
+$('img#eventr').on('mouseout', function(e){
   $('#eventr-modal').toggle();
 })
