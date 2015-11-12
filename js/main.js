ba60f2a33a6a02a1c4322695d8b90bd3e74fdcd3
Benjamin Aplin <benjaminaplin@gmail.com>
2015-11-12 10:03:04 -0500

    add hover modal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log('main is linked');
+
+$('div.eventr').on('mouseover', function(e){
+  $('img#eventr').css('opacity','0.3');
+  $('#eventr-modal').toggle();
+})
+
+$('div.eventr').on('mouseout', function(e){
+  $('img#eventr').css('opacity','1');
+  $('#eventr-modal').toggle();
+})
