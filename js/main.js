ea1555876e8253f172e341ce9a42816453b408fa
Benjamin Aplin <benjaminaplin@gmail.com>
2015-11-15 21:11:06 -0500

    fix eventr modal w CSS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 console.log('main is linked');
 
-$('div.eventr').on('mouseover', function(e){
-  $('#eventr-modal').toggle();
-})
+// document.querySelector('div.eventr').addEventListener('mouseover', function(e){
+//   console.log("mouseover", e.fromElement);
+//   if (e.fromElement !== 'P'){
+//     $('div#eventr-modal').toggle();
+  
+//     e.target.addEventListener('mouseout', function handler(d){
+//       console.log("mouseout", d.target);
+//       $('div#eventr-modal').toggle();
+//       this.removeEventListener('mouseout', handler, false);
+//     }, false);
+//   }
 
-$('img#eventr').on('mouseout', function(e){
-  $('#eventr-modal').toggle();
-})
+// }, false);
